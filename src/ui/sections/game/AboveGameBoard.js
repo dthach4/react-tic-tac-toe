bebbac9ee6f85033a052d347b3f0ec2fc011cd8a
Dany Thach <d.thach4@gmail.com>
2022-06-28 19:26:26 +0200

    Whoops it should be className mb

--- src/ui/sections/game/AboveGameBoard.js
+++ src/ui/sections/game/AboveGameBoard.js
@@ -3,7 +3,7 @@ import './AboveGameBoard.css';
 
 export default function AboveGameBoard() {
   return (
-    <div class="AboveGameBoard">
+    <div className="AboveGameBoard">
       <h1>Tic-Tac-Toe</h1>
     </div>
   )
